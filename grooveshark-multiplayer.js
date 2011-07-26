1f871c95fd4099f139f31e816b5c0e71c73b387f
Henrik Haugbølle <henrikhaugboelle@live.dk>
2011-07-26 17:38:46 +0200

    setting up the basic server

--- grooveshark-multiplayer.js
+++ grooveshark-multiplayer.js
@@ -1,23 +1,27 @@
 (function () {
 	if (window.Toastbread) {
-		if (!window.io) {
-			var ioinclude;
-			(ioinclude=document.createElement('script')).src='http://cdn.socket.io/stable/socket.io.js';
-			document.body.appendChild(ioinclude);
-		}
-	
 		var GSMP = {
-			mode: 'synch',
+			mode: 'sync',
+			socket: Object,
 			init: function() {
 				console.log('Grooveshark Multiplayer loaded');
+
+				console.log(window);
+				
+				GSMP.socket = new io.connect('http://192.168.1.16:8080/gsmp');
+
 				Toastbread.onSetVolume(function(volume) {
 					console.log("GSMP registed volume change to"+volume);
+
+					GSMP.socket.emit('method', { 'volume': volume });
 				});
 			}
 		}
-		
-		window.Grooveshark.Multiplayer = GSMP;
-		GSMP.init();
+
+		setTimeout(function() {
+			window.Grooveshark.Multiplayer = GSMP;
+			GSMP.init();
+		}, 500);
 	} else {
 		console.log("Grooveshark Multiplayer is dependent on Toastbread (Grooveshark Javascript API Extension)");
 	}	
