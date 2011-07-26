ef149c73e3a0c7cf70e055e3808e653c09b82a5d
Mick Hansen <doomschild@gmail.com>
2011-07-26 15:33:06 +0200

    Add socket.io include

--- grooveshark-multiplayer.js
+++ grooveshark-multiplayer.js
@@ -1,6 +1,13 @@
 (function () {
 	if (window.Toastbread) {
+		if (!window.io) {
+			var ioinclude;
+			(ioinclude=document.createElement('script')).src='http://cdn.socket.io/stable/socket.io.js';
+			document.body.appendChild(ioinclude);
+		}
+	
 		var GSMP = {
+			mode: 'synch',
 			init: function() {
 				console.log('Grooveshark Multiplayer loaded');
 				Toastbread.onSetVolume(function(volume) {
